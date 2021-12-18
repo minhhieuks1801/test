a131522e1009356b316e9aa1bcba5a4440c90fa4
minhhieuks1801 <86782660+minhhieuks1801@users.noreply.github.com>
2021-12-18 14:50:15 +0700

    /

---
+++ Ictshop/Controllers/UserController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ictshop.Models;
+namespace Ictshop.Controllers
+{
+    public class UserController : Controller
+    {
+        Qlbanhang db = new Qlbanhang();
+        // ĐĂNG KÝ
+        public ActionResult Dangky()
+        {
+            return View();
+        }
+
+        // ĐĂNG KÝ PHƯƠNG THỨC POST
+        [HttpPost]
+        public ActionResult Dangky(Nguoidung nguoidung)
+        {
+            try
+            {
+                // Thêm người dùng  mới
+                db.Nguoidungs.Add(nguoidung);
+                // Lưu lại vào cơ sở dữ liệu
+                db.SaveChanges();
+                // Nếu dữ liệu đúng thì trả về trang đăng nhập
+                if (ModelState.IsValid)
+                    {
+                        return RedirectToAction("Dangnhap");
+                    }
+                return View("Dangky");
+                
+            }
+            catch
+            {
+                return View();
+            }
+        }
+   
+        public ActionResult Dangnhap()
+        {
+            return View();
+
+        }
+
+
+        [HttpPost]
+        public ActionResult Dangnhap(FormCollection userlog)
+        {
+            string userMail = userlog["userMail"].ToString();
+            string password = userlog["password"].ToString();
+            var islogin = db.Nguoidungs.SingleOrDefault(x => x.Email.Equals(userMail) && x.Matkhau.Equals(password));
+
+            if (islogin != null)
+                {
+                    if (GetIDGroub(userMail) == "Admin")
+                        {
+                           Session["use"] = islogin;
+                           return RedirectToAction("Index", "Admin/Home");
+                        }
+                     else
+                         {
+                           Session["use"] = islogin;
+                           return RedirectToAction("Index","Home");
+                         }
+                 }
+            else
+                {
+                    ViewBag.Fail = "Đăng nhập thất bại";
+                    return View("Dangnhap");
+                }
+
+        }
+
+        public string GetIDGroub(string userMail)
+        {
+            var data = (from a in db.Nguoidungs
+                        join b in db.PhanQuyens on a.IDQuyen equals b.IDQuyen
+                        where a.Email == userMail
+                        select b);
+            if (data != null)
+            {
+                foreach(var item in data)
+                {
+                    return item.TenQuyen;
+                }
+            }
+            return "not";
+                  
+        }
+        public ActionResult DangXuat()
+        {
+            Session["use"] = null;
+            return RedirectToAction("Index","Home");
+
+        }
+
+
+    }
+}
